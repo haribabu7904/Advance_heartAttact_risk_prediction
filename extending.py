c6f555aa051bb3271b6031b35a0fde69cde975a9
haribabu7904 <haribabu790484@gmail.com>
2026-08-18 18:01:58 +0530

    Add files via upload

--- extending.py
+++ extending.py
@@ -1,7 +1,3 @@
-"""
-Added for symmetry with the core API
-"""
-
-from numba.core.extending import intrinsic as _intrinsic
-
-intrinsic = _intrinsic(target='cuda')
+# Re-export symbols
+from numba.core.extending import *  # noqa: F403, F401
+from numba.core.extending import _Intrinsic  # noqa: F401
